d7a575879e8a0d12c460df801855584e0321f9fe
santhosh0703 <santhoshrama007@gmail.com>
2020-10-21 22:01:26 +0530

    s

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,30 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
+  render() {
+    const [par, setPara] = useState("");
+    function handleClick() {
+      setPara(
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      );
     }
+    return (
+      <div id="main">
+        {par}
+        <button id="click" onClick={handleClick}>
+          Button
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
