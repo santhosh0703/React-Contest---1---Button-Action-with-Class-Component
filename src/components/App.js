0d1edfa50b067ecfb265c292660d49a52bf5699f
santhosh0703 <santhoshrama007@gmail.com>
2020-10-21 23:02:14 +0530

    ff

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      text: ""
+    };
   }
 
   render() {
@@ -14,12 +16,14 @@ class App extends Component {
           <button
             id="click"
             onClick={() =>
-              this.setState(
-                <p id="para">
-                  Hello, I've learnt to use the full-stack evaluation tool. This
-                  makes me so happy
-                </p>
-              )
+              this.setState({
+                text: (
+                  <p id="para">
+                    Hello, I've learnt to use the full-stack evaluation tool.
+                    This makes me so happy
+                  </p>
+                )
+              })
             }
           >
             Button
